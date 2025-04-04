1cdd3c360580fee877b51da5d2ac0f7b58911108
Eduardo Liborio <edupedro.eng@gmail.com>
2025-04-03 22:48:57 -0300

    Implementação da classe Lista

--- SimuladorLixo/src/application/Main.java
+++ SimuladorLixo/src/application/Main.java
@@ -1,10 +1,20 @@
 package application;
 
+import estruturasDeDados.Fila;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Fila fila = new Fila();
+		
+		fila.enqueue(10);
+		fila.enqueue(20);
+		fila.enqueue(30);
+		fila.imprimir();
+		
+		fila.dequeue();
+		fila.dequeue();
+		fila.imprimir();
 	}
 
 }
--- SimuladorLixo/src/estruturasDeDados/Fila.java
+++ SimuladorLixo/src/estruturasDeDados/Fila.java
@@ -1,5 +1,68 @@
 package estruturasDeDados;
 
 public class Fila {
-
+	private No head;
+	private No tail;
+	private int tamanho;
+	
+	private class No {
+		int valor;
+		No prox;
+		
+		No(int valor){
+			this.valor = valor;
+			this.prox = null;
+		}
+	}
+	
+	public Fila() {
+		head = null;
+		tail = null;
+		tamanho = 0;
+	}
+	
+	public int tamanho() {
+		return tamanho;
+	}
+	
+	public No enqueue(int item) {
+		No novoNo = new No (item);
+		if (head == null) {
+			head = novoNo;
+		} else {
+			tail.prox = novoNo;
+		}
+		tail = novoNo;
+		tamanho++;
+		return head;
+	}
+	
+	public No dequeue() {
+		if(tail == null) {
+			throw new RuntimeException("A Fila está vazia");
+		} else if(head.prox == null) {
+				head.prox = null;
+				tail.prox = null;
+		} else {
+			head = head.prox;
+		}
+		return head;
+	}
+	
+	public void imprimir() {
+		No atual = head;
+		System.out.print("Fila: ");
+		while (atual != null) {
+			System.out.print(atual.valor + " ");
+			atual = atual.prox;
+		}
+	}
+	
 }
+
+
+
+
+
+
+
